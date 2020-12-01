f9e60de28e8f8f5e11c11094713cb60a8d76ec91
mattenbar <matthw.enbar@gmail.com>
2020-12-01 11:51:11 -0500

    getting started

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import thunk from 'redux-thunk'
 import App from './App';
+import reducer from './reducers/index'
+import {BrowserRouter as Router} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+export default function Reducer(state = [], action) {
+  
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+}
